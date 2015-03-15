77f75fb023a493c67f36d86bc5df3dbc602ac587
林军 <lj@strongsoft.net>
2015-03-15 23:18:52 +0800

    new file

---
+++ www/baidupush.js
@@ -0,0 +1,27 @@
+var cordova = require('cordova'),
+    exec = require('cordova/exec');
+
+var BaiduPush = function() {
+    this.registered = false;
+};
+BaiduPush.prototype.init = function(api_key)
+{
+    exec(baiduPush.successFn, baiduPush.failureFn, 'BaiduPush', 'init', [api_key]);
+};
+
+BaiduPush.prototype.successFn = function(info)
+{
+    if(info){
+        baiduPush.registered = true;
+        cordova.fireDocumentEvent("cloudPushRegistered", info);
+    }
+};
+
+BaiduPush.prototype.failureFn = function(info)
+{
+    baiduPush.registered = false;
+};
+
+var baiduPush = new BaiduPush();
+
+module.exports = baiduPush;
